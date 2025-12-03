54060163613c85c1cce4f373536ee129cf707666
Phung Anh Van <phunganhvan13122004@gmail.com>
2025-12-03 22:43:24 +0700

    lay data

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ console.log("Welcome to the Music App!");
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import * as database from './config/database';
+import Topic from './models/topic.model';
 
 dotenv.config();
 // config env
@@ -23,8 +24,11 @@ app.set(`views`, `./views`);
 app.set(`view engine`, `pug`);
 
 
-app.get('/topics', (req: Request, res: Response) => {
+app.get('/topics', async (req: Request, res: Response) => {
     // res.send('Danh sách các chủ đề bài hát');
+    const topics = await Topic.find({
+        deleted: false,
+    })
     res.render('client/pages/topics/index');
 });
 
