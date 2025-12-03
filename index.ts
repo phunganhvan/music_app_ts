8af6ae846034cfbda811d42154020d01e8a853e9
Phung Anh Van <phunganhvan13122004@gmail.com>
2025-12-03 16:00:07 +0700

    Add basic router

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
 console.log("Welcome to the Music App!");
+
+
+// localhost:3000/topics chủ đề bài hát
+
+import express, { Express, Request, Response } from 'express';
+const app : Express = express();
+const port: number =3000;
+
+app.get('/topics', (req: Request, res: Response) => {
+    res.send('Danh sách các chủ đề bài hát');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
